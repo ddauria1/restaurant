17faf239772dfb26686db4ebe659391bd1f3b2c2
danilodauria1@gmail.com <pinturicchio10>
2020-05-30 15:00:23 +0100

    basement for layout

---
+++ resources/views/admin/layout/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Restaurant</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <style>
+        label{ display: block; }
+    </style>
+</head>
+<body>
+<div>
+    <h1>{{$title}}</h1>
+    <div id="mainContent">
+
+    </div>
+</div>
+</body>
+</html>
